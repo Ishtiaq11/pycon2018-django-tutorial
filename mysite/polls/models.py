70234782fba31ea2e36675674682b6c96359727e
Ishtiaq Ahmed <ishtiaqpias@gmail.com>
2020-03-22 23:21:03 +0600

    Add model comment

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -36,4 +36,11 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.user.username
+        
+        
+class Comment(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
     
